4de39cd34e361d6cbe717ba4c28404e4093249d0
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2024-02-07 20:46:10 +0100

    Add various packages + some default files

---
+++ UnholyFrog_Source/Assets/Sources/Components/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace UnholyFrog.Game.Components
+{
+    public class PlayerController : MonoBehaviour
+    {
+        /// <summary>
+        /// Settings for the player controller
+        /// </summary>
+        public PlayerControllerSettings settings;
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ UnholyFrog_Source/Assets/Sources/Settings/PlayerControllerSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UnholyFrog.Game.Components
+{
+    [CreateAssetMenu(fileName = nameof(PlayerControllerSettings), menuName = "Settings/PlayerController")]
+    public class PlayerControllerSettings : ScriptableObject
+    {
+        /// <summary>
+        /// Avatar speed
+        /// </summary>
+        public float Speed;
+    }
+}
